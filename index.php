fa08a271a4c0cabc221069ad61d8a893796edc61
Kelly Lougheed <kellylougheed@gmail.com>
2018-04-06 17:18:01 -0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Kelly Lougheed</title>
+    <meta name="description" content="Kelly Lougheed is a computer science educator, software developer, and writer in Los Angeles, CA.">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
+    <link href='https://fonts.googleapis.com/css?family=Abril+Fatface' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="materialize.min.css">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://use.fontawesome.com/ee14bb1f23.js"></script>
+  </head>
+  <body>
+  	<h1>Kelly Lougheed</h1>
+
+  	<p><a class="contact" href="https://medium.com/@kellylougheed" target="_blank"><i class="fa fa-medium" aria-hidden="true"></i></a> <a class="contact" href="https://github.com/kellylougheed" target="_blank"><i class="fa fa-github" aria-hidden="true"></i></a> <a class="contact" href="https://www.linkedin.com/in/kellylougheed/" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a> <a class="contact" href="http://www.twitter.com/kellylougheed" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a> <a class="contact" href="mailto:hello@kellylougheed.com?Subject=Hello"><i class="fa fa-envelope-o" aria-hidden="true"></i></a></p>
+
+  	<div class="parent">
+
+  	<div>
+  		<h4>Software Developer</h4>
+  		<div class="card">
+        <div class="card-image">
+          <img src="images/developer.png">
+        </div>
+        <div class="card-content">
+          <p>I am earning my certificate in Software Development from UC Berkeley and have worked as a developer using Python, JavaScript, and PHP. I have also studied Java, C, Ruby on Rails, and Swift.</p>
+        </div>
+        <div class="card-action">
+          <a href="http://kellylougheed.com/dev/" target="_blank">Software Portfolio</a>
+        </div>
+  		</div>
+  	</div>
+
+  	<div>
+  	<h4>Educator</h4>
+      <div class="card">
+        <div class="card-image">
+          <img src="images/educator.png">
+        </div>
+        <div class="card-content">
+          <p>I have taught computer science and Latin to grades 6-12 in CA, FL, and AZ. I am currently finishing my certificate in Computer Science Education at the College of St. Scholastica.</p>
+        </div>
+        <div class="card-action">
+          <a href="http://www.kellylougheed.com/teaching/" target="_blank">Teaching Portfolio</a>
+        </div>
+      </div>
+     </div>
+
+    <div>
+  	<h4>Technical Writer</h4>
+      <div class="card">
+        <div class="card-image">
+          <img src="images/writer.png">
+        </div>
+        <div class="card-content">
+          <p>I write coding tutorials for a variety of levels about web development, WebVR, p5.js, basic programming, and application development with Java.</p>
+        </div>
+        <div class="card-action">
+          <a href="https://medium.com/@kellylougheed" target="_blank">Medium</a>
+        </div>
+      </div>
+     </div>
+
+     <div>
+     <h4>Author</h4>
+      <div class="card">
+        <div class="card-image">
+          <img src="images/author.png">
+        </div>
+        <div class="card-content">
+          <p>I have published four young adult novels. Based on my own experience at an all-girls school, these books satirize contemporary YA vampire stories.</p>
+        </div>
+        <div class="card-action">
+          <a href="http://www.kellylougheed.com/author/" target="_blank">Author Website</a>
+        </div>
+      </div>
+      </div>
+
+  </div>
+
+  <footer>© Kelly Lougheed <?php echo date("Y"); ?>. All rights reserved.</footer>
+
+  </body>
+</html>
